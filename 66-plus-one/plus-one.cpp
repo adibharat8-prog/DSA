d000a164b0b5452a8b5849d440616f223457430d
ADITYA KUMAR <adibharat8@gmail.com>
2026-09-06 23:10:53 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (26.14%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        vector<int> ans(digits.size()+1);
+        
         int n = digits.size();
         int val = n-1;
         bool empty = false;
@@ -15,39 +15,24 @@ public:
             }
         }
 
-        cout<<val<<endl;
         digits[val]++;
         val++;
-        cout<<"val"<<val<<endl;
         for(int i=val; i<digits.size(); i++){
-            cout<<i<<endl;
             digits[i] = 0;
         }
 
 
-        if(empty){            
+        if(empty){          
+            vector<int> ans(digits.size()+1);  
             ans[val-1] = 1;
             val;
             for(int i=val; i<ans.size(); i++){
                 
                 ans[i] = 0;
             }
+            return ans;
         }
 
-        return (empty)? ans : digits;
+        return digits;
     }
 };
-
-// vector<int> ans;
-//         long long d = 0;
-//         for(int i=0; i<digits.size(); i++){
-//             d = d*10 + digits[i];
-//         }
-//         d++;
-//         while(d>0){
-//             int dt = d%10;
-//             ans.push_back(dt);
-//             d = d /10;
-//         }
-//         reverse(ans.begin(),ans.end());
-//         return ans;
